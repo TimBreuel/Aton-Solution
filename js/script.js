7c7905d4db849af5f77b56dcff8f58331264de30
TimBreuel <55285070+TimBreuel@users.noreply.github.com>
2019-10-02 14:37:07 +0200

    Last Steps
    Fast fertig, nur noch ein paar kleinigkeiten. Js macht noch Probleme.

--- js/script.js
+++ js/script.js
@@ -7,4 +7,20 @@ function mobileNavMenuJs() {
     }  
 }
 
-console.log('Warum gehst du nicht');
+ let myButton = document.getElementById('myBtn');
+
+ window.onscroll = function() {
+     scrollFunction()
+ };
+ function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
