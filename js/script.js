776ebcff93d04d142dafeccc700321a4160ee6ae
TimBreuel <55285070+TimBreuel@users.noreply.github.com>
2019-12-13 15:28:39 +0100

    Border.radius-function

--- js/script.js
+++ js/script.js
@@ -1,26 +1,41 @@
 function mobileNavMenuJs() {
-    let x = document.getElementById('mobile-nav-menu-js');
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }  
+  let x = document.getElementById('mobile-nav-menu-js');
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
 
- let myButton = document.getElementById('myBtn');
+let myButton = document.getElementById('myBtn');
 
- window.onscroll = function() {
-     scrollFunction()
- };
- function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-  }
+window.onscroll = function () {
+  scrollFunction()
+};
 
-  function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// Ahmad fragen /////////////////////////////////////////////////
+// Toggle möglich? //////////////////////////////////////////
+// Warum funktioniert die nur bei einem
+let removeID = document.querySelector('.removeID');
+
+removeID.onclick = function () {
+  const removeFunction = document.querySelector('.removeID');
+  if (removeFunction.classList.contains('borderRadiusStart')) {
+    removeFunction.classList.remove('borderRadiusStart');
+  } else {
+    removeFunction.classList.add('borderRadiusStart');
+  };
+}
